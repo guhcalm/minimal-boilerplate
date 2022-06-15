5be4d517595cb027be588f781929d3b7ea5352c7
guhcalm <guhcalm@outlook.com>
2022-06-15 12:06:42 -0400

    main

--- src/context/reducers/index.ts
+++ src/context/reducers/index.ts
@@ -2,8 +2,13 @@ import { ActionsEnum, StateInterface } from "../../interfaces"
 import { ThemesStyles } from "../../styles"
 
 const { SET_LIGHT_THEME, SET_DARK_THEME } = ActionsEnum
+// initial state
+export const initialState: StateInterface = {
+  theme: ThemesStyles.Light
+}
 const meta = document.querySelector("[name='theme-color']") as HTMLMetaElement
-
+meta.content = ThemesStyles.Dark.palette.background.main
+// reducers
 const reducers = {
   [SET_LIGHT_THEME]: state => {
     meta.content = ThemesStyles.Light.palette.background.main
@@ -14,17 +19,13 @@ const reducers = {
     return { ...state, theme: ThemesStyles.Dark }
   }
 }
-
+// reducer
 export default (
   state: StateInterface,
   action: { type: ActionsEnum; payload: any }
 ) => reducers[action.type](state, action)
-
+// actions
 export const actions = {
   [SET_LIGHT_THEME]: () => ({ type: SET_LIGHT_THEME }),
   [SET_DARK_THEME]: () => ({ type: SET_DARK_THEME })
-}
-
-export const initialState: StateInterface = {
-  theme: ThemesStyles.Light
 }
