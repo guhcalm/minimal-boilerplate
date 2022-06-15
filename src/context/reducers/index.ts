b27df49331d44833f7c06e92db1ea0ed79aeddd7
guhcalm <guhcalm@outlook.com>
2022-06-15 12:02:56 -0400

    main

--- src/context/reducers/index.ts
+++ src/context/reducers/index.ts
@@ -2,10 +2,17 @@ import { ActionsEnum, StateInterface } from "../../interfaces"
 import { ThemesStyles } from "../../styles"
 
 const { SET_LIGHT_THEME, SET_DARK_THEME } = ActionsEnum
+const meta = document.querySelector("[name='theme-color']") as HTMLMetaElement
 
 const reducers = {
-  [SET_LIGHT_THEME]: state => ({ ...state, theme: ThemesStyles.Light }),
-  [SET_DARK_THEME]: state => ({ ...state, theme: ThemesStyles.Dark })
+  [SET_LIGHT_THEME]: state => {
+    meta.content = ThemesStyles.Light.palette.background.main
+    return { ...state, theme: ThemesStyles.Light }
+  },
+  [SET_DARK_THEME]: state => {
+    meta.content = ThemesStyles.Dark.palette.background.main
+    return { ...state, theme: ThemesStyles.Dark }
+  }
 }
 
 export default (
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react"
 import { useParams } from "react-router-dom"
+import { useCustomContext } from "../../hooks"
 import Container from "./styles"
 
 export default () => {
   const { id } = useParams()
+  const { dispatch, actions } = useCustomContext()
+  useEffect(() => {
+    dispatch(actions.setLightTheme())
+  }, [])
   return <Container data-home="">Home Page {id}</Container>
 }
